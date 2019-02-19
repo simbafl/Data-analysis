613a186a6e1da5e61b590da2e5e0d852ddd3e805
fenglei110 <1652997150@qq.com>
2019-02-19 20:19:17 +0800

    验证码破解

--- Spider/ch_Code/README.md
+++ Spider/ch_Code/README.md
@@ -1,2 +1,13 @@
 # 滑动验证码破解示例
-由『国家企业信用信息公示系统』进行滑动破解体验
+通过Selenium模拟用户滑动解锁    
+由『国家企业信用信息公示系统』进行破解体验
+
+具体步骤：
+1. 使用Selenium打开页面 
+2. 匹配到输入框，输入要查询的信息，并点击查询按钮
+3. 读取验证码图片，并做缺口识别
+4. 根据缺口位置，计算滑动距离
+5. 根据滑动距离，拖拽滑块到需要匹配的位置
+
+
+![spider-1](https://github.com/fenglei110/Data-analysis/blob/master/Spider/ch_Bilibili/images/spider_image.gif)
--- Spider/ch_Code/sliding_code.py
+++ Spider/ch_Code/sliding_code.py
@@ -1,166 +1,230 @@
-# -*-coding:utf-8-*-
-import requests
-import re
-import StringIO
-from PIL import Image
+# -*-coding:utf-8 -*-
 import random
-import math
 import time
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+from urllib.request import urlretrieve
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import PIL.Image as image
+import re
 
 
-class crack_picture(object):
-    def __init__(self, img_url1, img_url2):
-        self.img1, self.img2 = self.picture_get(img_url1, img_url2)
+class Crack():
+    def __init__(self,keyword):
+        self.url = 'http://bj.gsxt.gov.cn/sydq/loginSydqAction!sydq.dhtml'
+        self.browser = webdriver.Chrome('D:\\chromedriver.exe')
+        self.wait = WebDriverWait(self.browser, 100)
+        self.keyword = keyword
+        self.BORDER = 6
 
-    def picture_get(self, img_url1, img_url2):
-        hd = {"Host": "static.geetest.com",
-              "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
-        img1 = StringIO.StringIO(self.repeat(img_url1, hd).content)
-        img2 = StringIO.StringIO(self.repeat(img_url2, hd).content)
-        return img1, img2
+    def open(self):
+        """
+        打开浏览器,并输入查询内容
+        """
+        self.browser.get(self.url)
+        keyword = self.wait.until(EC.presence_of_element_located((By.ID, 'keyword_qycx')))
+        button = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'btn')))
+        keyword.send_keys(self.keyword)
+        button.click()
 
-    def repeat(self, url, hd):
-        """请求url"""
-        times = 10
-        while times > 0:
-            try:
-                ans = requests.get(url, headers=hd)
-                return ans
-            except:
-                times -= 1
+    def get_images(self, bg_filename='bg.jpg', fullbg_filename='fullbg.jpg'):
+        """
+        获取验证码图片
+        :return: 图片的location信息
+        """
+        bg = []
+        fullgb = []
+        while bg == [] and fullgb == []:
+            bf = BeautifulSoup(self.browser.page_source, 'lxml')
+            bg = bf.find_all('div', class_ = 'gt_cut_bg_slice')
+            fullgb = bf.find_all('div', class_ = 'gt_cut_fullbg_slice')
+        bg_url = re.findall('url\(\"(.*)\"\);', bg[0].get('style'))[0].replace('webp', 'jpg')
+        fullgb_url = re.findall('url\(\"(.*)\"\);', fullgb[0].get('style'))[0].replace('webp', 'jpg')
+        bg_location_list = []
+        fullbg_location_list = []
+        for each_bg in bg:
+            location = dict()
+            location['x'] = int(re.findall('background-position: (.*)px (.*)px;',each_bg.get('style'))[0][0])
+            location['y'] = int(re.findall('background-position: (.*)px (.*)px;',each_bg.get('style'))[0][1])
+            bg_location_list.append(location)
+        for each_fullgb in fullgb:
+            location = dict()
+            location['x'] = int(re.findall('background-position: (.*)px (.*)px;',each_fullgb.get('style'))[0][0])
+            location['y'] = int(re.findall('background-position: (.*)px (.*)px;',each_fullgb.get('style'))[0][1])
+            fullbg_location_list.append(location)
 
-    def pictures_recover(self):
-        xpos = self.judge(self.picture_recover(self.img1, 'img1.jpg'), self.picture_recover(self.img2, 'img2.jpg')) - 6
-        return self.darbra_track(xpos)
+        urlretrieve(url=bg_url, filename=bg_filename)
+        print('缺口图片下载完成')
+        urlretrieve(url=fullgb_url, filename=fullbg_filename)
+        print('背景图片下载完成')
+        return bg_location_list, fullbg_location_list
 
-    def picture_recover(self, img, name):
-        a = [39, 38, 48, 49, 41, 40, 46, 47, 35, 34, 50, 51, 33, 32, 28, 29, 27, 26, 36, 37, 31, 30, 44, 45, 43, 42, 12,
-             13, 23, 22, 14, 15, 21, 20, 8, 9, 25, 24, 6, 7, 3, 2, 0, 1, 11, 10, 4, 5, 19, 18, 16, 17]
-        im = Image.open(img)
-        im_new = Image.new("RGB", (260, 116))
-        for row in range(2):
-            for column in range(26):
-                right = a[row * 26 + column] % 26 * 12 + 1
-                down = 58 if a[row * 26 + column] > 25 else 0
-                for w in range(10):
-                    for h in range(58):
-                        ht = 58 * row + h
-                        wd = 10 * column + w
-                        im_new.putpixel((wd, ht), im.getpixel((w + right, h + down)))
-        im_new.save(name)
-        return im_new
+    def get_merge_image(self, filename, location_list):
+        """
+        根据位置对图片进行合并还原
+        :filename:图片
+        :location_list:图片位置
+        """
+        im = image.open(filename)
+        new_im = image.new('RGB', (260, 116))
+        im_list_upper = []
+        im_list_down = []
 
-    def darbra_track(self, distance):
-        return [[distance, 0.5, 1]]
+        for location in location_list:
+            if location['y'] == -58:
+                im_list_upper.append(im.crop((abs(location['x']), 58, abs(location['x']) + 10, 166)))
+            if location['y'] == 0:
+                im_list_down.append(im.crop((abs(location['x']), 0, abs(location['x']) + 10, 58)))
 
-    def diff(self, img1, img2, wd, ht):
-        rgb1 = img1.getpixel((wd, ht))
-        rgb2 = img2.getpixel((wd, ht))
-        tmp = reduce(lambda x, y: x + y, map(lambda x: abs(x[0] - x[1]), zip(rgb1, rgb2)))
-        return True if tmp >= 200 else False
+        new_im = image.new('RGB', (260,116))
 
-    def col(self, img1, img2, cl):
-        for i in range(img2.size[1]):
-            if self.diff(img1, img2, cl, i):
-                return True
-        return False
+        x_offset = 0
+        for im in im_list_upper:
+            new_im.paste(im, (x_offset,0))
+            x_offset += im.size[0]
 
-    def judge(self, img1, img2):
-        for i in range(img2.size[0]):
-            if self.col(img1, img2, i):
-                return i
-        return -1
+        x_offset = 0
+        for im in im_list_down:
+            new_im.paste(im, (x_offset,58))
+            x_offset += im.size[0]
 
-class gsxt(object):
-    def __init__(self, br_name="phantomjs"):
-        self.br = self.get_webdriver(br_name)
-        self.wait = WebDriverWait(self.br, 10, 1.0)
-        self.br.set_page_load_timeout(8)
-        self.br.set_script_timeout(8)
+        new_im.save(filename)
 
-    def input_params(self, name):
-        self.br.get("http://www.gsxt.gov.cn/index")
-        element = self.wait_for(By.ID, "keyword")
-        element.send_keys(name)
-        time.sleep(1.1)
-        element = self.wait_for(By.ID, "btn_query")
-        element.click()
-        time.sleep(1.1)
+        return new_im
 
-    def drag_pic(self):
-        return (self.find_img_url(self.wait_for(By.CLASS_NAME, "gt_cut_fullbg_slice")),
-                self.find_img_url(self.wait_for(By.CLASS_NAME, "gt_cut_bg_slice")))
+    def is_pixel_equal(self, img1, img2, x, y):
+        """
+        判断两个像素是否相同
+        :param img1: 图片1
+        :param img2: 图片2
+        :param x: 位置x
+        :param y: 位置y
+        :return: 像素是否相同
+        """
+        # 取两个图片的像素点
+        pix1 = img1.load()[x, y]
+        pix2 = img2.load()[x, y]
+        threshold = 60
+        if (abs(pix1[0] - pix2[0] < threshold) and abs(pix1[1] - pix2[1] < threshold)
+                and abs(pix1[2] - pix2[2] < threshold)):
+            return True
+        else:
+            return False
 
-    def wait_for(self, by1, by2):
-        return self.wait.until(EC.presence_of_element_located((by1, by2)))
+    def get_gap(self, img1, img2):
+        """
+        获取缺口偏移量
+        :param img1: 不带缺口图片
+        :param img2: 带缺口图片
+        :return:
+        """
+        left = 43
+        for i in range(left, img1.size[0]):
+            for j in range(img1.size[1]):
+                if not self.is_pixel_equal(img1, img2, i, j):
+                    left = i
+                    return left
+        return left
 
-    def find_img_url(self, element):
-        try:
-            return re.findall('url\("(.*?)"\)', element.get_attribute('style'))[0].replace("webp", "jpg")
-        except:
-            return re.findall('url\((.*?)\)', element.get_attribute('style'))[0].replace("webp", "jpg")
+    def get_track(self, distance):
+        """
+        根据偏移量获取移动轨迹
+        :param distance: 偏移量
+        :return: 移动轨迹
+        """
+        # 移动轨迹
+        track = []
+        # 当前位移
+        current = 0
+        # 减速阈值
+        mid = distance * 4 / 5
+        # 计算间隔
+        t = 0.2
+        # 初速度
+        v = 0
 
-    def emulate_track(self, tracks):
-        element = self.br.find_element_by_class_name("gt_slider_knob")
-        ActionChains(self.br).click_and_hold(on_element=element).perform()
-        for x, y, t in tracks:
-            ActionChains(self.br).move_to_element_with_offset(
-                to_element=element,
-                xoffset=x + 22,
-                yoffset=y + 22).perform()
-            ActionChains(self.br).click_and_hold().perform()
-            time.sleep(t)
-        time.sleep(0.24)
-        ActionChains(self.br).release(on_element=element).perform()
-        time.sleep(0.8)
-        element = self.wait_for(By.CLASS_NAME, "gt_info_text")
-        ans = element.text
-        print ans
-        return ans
-
-    def run(self):
-        for i in [u'招商银行', u'交通银行', u'中国银行']:
-            self.hack_geetest(i)
-            time.sleep(1)
-        self.quit_webdriver()
-
-    def hack_geetest(self, company=u"招商银行"):
-        flag = True
-        self.input_params(company)
-        while flag:
-            img_url1, img_url2 = self.drag_pic()
-            tracks = crack_picture(img_url1, img_url2).pictures_recover()
-            tsb = self.emulate_track(tracks)
-            if u'通过' in tsb:
-                time.sleep(1)
-                soup = BeautifulSoup(self.br.page_source, 'html.parser')
-                for sp in soup.find_all("a", attrs={"class": "search_list_item"}):
-                    print re.sub("\s+", "", sp.get_text().encode("utf-8"))
-                break
-            elif u'吃' in tsb:
-                time.sleep(5)
+        while current < distance:
+            if current < mid:
+                # 加速度为正2
+                a = 2
             else:
-                self.input_params(company)
+                # 加速度为负3
+                a = -3
+            # 初速度v0
+            v0 = v
+            # 当前速度v = v0 + at
+            v = v0 + a * t
+            # 移动距离x = v0t + 1/2 * a * t^2
+            move = v0 * t + 1 / 2 * a * t * t
+            # 当前位移
+            current += move
+            # 加入轨迹
+            track.append(round(move))
+        return track
 
-    def quit_webdriver(self):
-        self.br.quit()
+    def get_slider(self):
+        """
+        获取滑块
+        :return: 滑块对象
+        """
+        while True:
+            try:
+                slider = self.browser.find_element_by_xpath("//div[@class='gt_slider_knob gt_show']")
+                break
+            except:
+                time.sleep(0.5)
+        return slider
 
-    def get_webdriver(self, name):
-        if name.lower() == "phantomjs":
-            dcap = dict(DesiredCapabilities.PHANTOMJS)
-            dcap["phantomjs.page.settings.userAgent"] = (
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
-            return webdriver.PhantomJS(desired_capabilities=dcap)
-        elif name.lower() == "chrome":
-            return webdriver.Chrome()
+    def move_to_gap(self, slider, track):
+        """
+        拖动滑块到缺口处
+        :param slider: 滑块
+        :param track: 轨迹
+        :return:
+        """
+        ActionChains(self.browser).click_and_hold(slider).perform()
+        while track:
+            x = random.choice(track)
+            ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
+            track.remove(x)
+        time.sleep(0.5)
+        ActionChains(self.browser).release().perform()
 
-if __name__ == "__main__":
-    gsxt("chrome").run()
+    def crack(self):
+        # 打开浏览器
+        self.open()
 
+        # 保存的图片名字
+        bg_filename = 'bg.jpg'
+        fullbg_filename = 'fullbg.jpg'
+
+        # 获取图片
+        bg_location_list, fullbg_location_list = self.get_images(bg_filename, fullbg_filename)
+
+        # 根据位置对图片进行合并还原
+        bg_img = self.get_merge_image(bg_filename, bg_location_list)
+        fullbg_img = self.get_merge_image(fullbg_filename, fullbg_location_list)
+
+        # 获取缺口位置
+        gap = self.get_gap(fullbg_img, bg_img)
+        print('缺口位置', gap)
+
+        track = self.get_track(gap-self.BORDER)
+        print('滑动滑块')
+        print(track)
+
+        # 点按呼出缺口
+        slider = self.get_slider()
+        # 拖动滑块到缺口处
+        self.move_to_gap(slider, track)
+
+
+if __name__ == '__main__':
+    print('开始验证')
+    crack = Crack(u'中国移动')
+    crack.crack()
+    print('验证成功')
